4f18e9ff0cfcfe05609260babf917365d5bfe281
Sebastian Russo <russo.sebastian@gmail.com>
2020-11-29 16:15:49 -0500

    added bash script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo Hello World 
+
+npm i aws-sdk body-parser concurrently cors dotenv express joi mongoose multer multer-s3 
+
+cd client
+npm i axios bootstrap config jquery node-modules react react-bootstrap react-dom react-scripts redux redux-form redux-thunk web-vitals 
+cd ..
+
+npm start
+
