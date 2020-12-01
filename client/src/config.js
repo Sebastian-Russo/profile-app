078e15a811afd67a6b9188b0580f530a50244863
Sebastian Russo <russo.sebastian@gmail.com>
2020-12-01 11:50:22 -0500

    started docker file

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,4 @@
 export const API_BASE_URL =
 // apps created with create-react-app to have environment variables flow through the build process, they must be prepended with REACT_APP_
-    process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
+    process.env.REACT_APP_API_BASE_URL ||'http://localhost:8080/api';
+    // process.env.REACT_APP_API_BASE_URL || 'mongodb://mongo:8080/api'
