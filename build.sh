34bd9c1b2cd975769637463be5d9ecf6f85f08df
Sebastian Russo <russo.sebastian@gmail.com>
2020-12-22 09:09:43 -0500

    updated bash and readme

--- build.sh
+++ build.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-echo Hello World 
 
-npm i concurrently
-npm i aws-sdk 
-npm i body-parser cors dotenv express joi mongoose 
-npm i multer multer-s3 
-npm i lodash
-npm i lodash.isempty
-npm i classnames
-npm i jwt-decode
+echo installing dependencies and starting server and client 
 
-cd client
-npm i bcryptjs passport passport-local passport-jwt jsonwebtoken 
-npm i bootstrap config jquery react react-bootstrap react-dom react-scripts redux redux-form redux-thunk react-router-dom 
-npm install --save prop-types
-npm i web-vitals axios
-npm i jwt-decode
+npm install
+
+cd client npm install
+
 cd ..
 
 npm start
